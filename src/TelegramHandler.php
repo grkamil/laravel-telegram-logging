4625cbe5697e08726cc35226f9c54c8e1c0c842a
Камиль Мухаметзянов <grkamil@yandex.ru>
2018-09-19 13:30:05 +0300

    telegram handler & logger;

---
+++ src/TelegramHandler.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Logging;
+
+use Exception;
+use Monolog\Logger;
+use Monolog\Handler\AbstractProcessingHandler;
+
+/**
+ * Class TelegramHandler
+ * @package App\Logging
+ */
+class TelegramHandler extends AbstractProcessingHandler
+{
+    /**
+     * Bot API token
+     *
+     * @var string
+     */
+    private $botToken;
+
+    /**
+     * Chat id for bot
+     *
+     * @var int
+     */
+    private $chatId;
+
+    /**
+     * Application name
+     *
+     * @string
+     */
+    private $appName;
+
+    /**
+     * Application environment
+     *
+     * @string
+     */
+    private $appEnv;
+
+    /**
+     * TelegramHandler constructor.
+     * @param int $level
+     */
+    public function __construct($level = Logger::DEBUG)
+    {
+        parent::__construct($level, true);
+
+        // define variables for making Telegram request
+        $this->botToken = env('TELEGRAM_LOGGER_BOT_TOKEN');
+        $this->chatId   = env('TELEGRAM_LOGGER_CHAT_ID');
+
+        // define variables for text message
+        $this->appName = config('app.name');
+        $this->appEnv  = config('app.env');
+    }
+
+    /**
+     * @param array $record
+     */
+    public function write(array $record)
+    {
+        if(!$this->botToken || !$this->chatId) {
+            return;
+        }
+
+        // trying to make request and send notification
+        try {
+            file_get_contents(
+                'https://api.telegram.org/bot' . $this->botToken . '/sendMessage?'
+                . http_build_query([
+                    'text' => $this->formatText($record['formatted'], $record['level_name']),
+                    'chat_id' => $this->chatId,
+                    'parse_mode' => 'html'
+                ])
+            );
+        } catch (Exception $exception) {
+
+        }
+    }
+
+    /**
+     * @param string $text
+     * @param string $level
+     * @return string
+     */
+    private function formatText(string $text, string $level): string
+    {
+        return '<b>' . $this->appName .  '</b> (' . $level . ')' . PHP_EOL  . 'Env: ' . $this->appEnv . PHP_EOL . $text;
+    }
+}
---
+++ src/TelegramLogger.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Logging;
+
+use Monolog\Logger;
+
+/**
+ * Class TelegramLogger
+ * @package App\Logging
+ */
+class TelegramLogger
+{
+    /**
+     * Create a custom Monolog instance.
+     *
+     * @param  array  $config
+     * @return \Monolog\Logger
+     */
+    public function __invoke(array $config)
+    {
+        return new Logger(
+            env('APP_NAME'),
+            [
+                new TelegramHandler()
+            ]
+        );
+    }
+}
